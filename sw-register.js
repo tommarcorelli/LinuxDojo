33553bcc62b18d678ee3b4002fdbef8772ad4315
Tom Marcorelli <tommarcorelli@gmail.com>
2026-07-02 00:45:14 +0200

    Delete sw-register.js

--- sw-register.js
+++
@@ -1,8 +0,0 @@
-// sw-register.js
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker.register("sw.js")
-      .then(() => console.log("[LinuxDojo] Service Worker enregistré"))
-      .catch(e => console.warn("[LinuxDojo] SW non enregistré :", e));
-  });
-}
